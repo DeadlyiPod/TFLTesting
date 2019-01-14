c9f2d424c7bbac89792bb1c37769b01784e5793d
John Braddock <johnb22@cfl.rr.com>
2019-01-14 13:16:50 -0500

    Test Opmode (nothng in it)
    Fixed RedDepot by changing encoder drive to use functional encoders :P

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JohnTest.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by johnb on 1/11/2019.
+ */
+@Disabled
+@Autonomous(name = "JohnTest", group = "a")
+public class JohnTest extends LinearOpMode{
+
+    //Initialize motors/servos
+    private DcMotor leftInnerDrive = null;
+    private DcMotor leftOuterDrive = null;
+
+    private DcMotor rightInnerDrive = null;
+    private DcMotor rightOuterDrive = null;
+
+    private DcMotor slideMotor = null;
+    private DcMotor rotateMotor = null;
+
+    private CRServo intake = null;
+    private Servo outtake = null;
+
+
+
+    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+
+    // These constants define the desired driving/control characteristics
+    // The can/should be tweaked to suite the specific robot drive train.
+    static final double     DRIVE_SPEED             = 0.7;     // Nominal speed for better accuracy.
+    static final double     TURN_SPEED              = 0.5;     // Nominal half speed for better accuracy.
+
+    static final double     HEADING_THRESHOLD       = 1 ;      // As tight as we can make it with an integer gyro
+    static final double     P_TURN_COEFF            = 0.1;     // Larger is more responsive, but also less stable
+    static final double     P_DRIVE_COEFF           = 0.15;     // Larger is more responsive, but also less stable
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+
+
+    public void gyroDrive (double speed,
+                           double distance,
+                           double angle) {
+        int     newLeftTarget;
+        int     newRightTarget;
+        int     moveCounts;
+        double  max;
+        double  error;
+        double  steer;
+        double  leftSpeed;
+        double  rightSpeed;
+
+        if (opModeIsActive()) {
+
+            //Determine new target position, and pass to motor controller
+            moveCounts = (int)(distance * COUNTS_PER_INCH);
+            newLeftTarget = leftOuterDrive.getCurrentPosition() + moveCounts;
+            newRightTarget = rightOuterDrive.getCurrentPosition() + moveCounts;
+        }
+    }
+}
